094ce41b1275d11419b84cb020f6d7292e6bb14d
Matouš Vrba <vrbamato@fel.cvut.cz>
2018-10-30 19:00:01 -0400

    small changes

--- src/BalloonPlanner.cpp
+++ src/BalloonPlanner.cpp
@@ -93,42 +93,6 @@ void BalloonPlanner::onInit()
 
 //}
 
-/* /1* callbackBalloonCloud() //{ *1/ */
-
-/* void BalloonPlanner::callbackBalloonCloud(const pcl::PointCloud<pcl::PointXYZ>::Ptr& msg) { */
-
-/*   if (!m_is_initialized) */
-/*     return; */
-
-/*   ROS_DEBUG_THROTTLE(1.0, "[%s]: Obtained balloon cloud", m_node_name); */
-
-/*   mrs_lib::Routine profiler_routine = m_profiler->createRoutine("callbackBalloonCloud"); */
-
-/*   ROS_INFO("[%s]: balloon cloud size: %lu", m_node_name, msg->size()); */
-
-/* } */
-
-/* //} */
-
-/* /1* //{ callbackReconfigure() *1/ */
-
-/* /1* void BalloonPlanner::callbackReconfigure([[maybe_unused]] balloon_planner::bpConfig& config, [[maybe_unused]] uint32_t level) { *1/ */
-
-/* /1*   if (!m_is_initialized) *1/ */
-/* /1*     return; *1/ */
-
-/* /1*   ROS_INFO( *1/ */
-/* /1*       "Reconfigure Request: " *1/ */
-/* /1*       "median_blur: %d, scale: %f, range_min: %f, range_max: %f", *1/ */
-/* /1*       config.median_blur, config.scale, config.range_min, config.range_max); *1/ */
-
-/* /1*   _median_blur = config.median_blur; *1/ */
-/* /1*   _scale       = config.scale; *1/ */
-/* /1*   _range_min   = config.range_min; *1/ */
-/* /1*   _range_max   = config.range_max; *1/ */
-/* /1* } *1/ */
-/* //} */
-
 }  // namespace balloon_planner
 
 #include <pluginlib/class_list_macros.h>
