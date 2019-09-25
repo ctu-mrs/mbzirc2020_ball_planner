6c0da7e6ea6ed37e05e3782950210b6da0cb0a02
Matouš Vrba <vrbamato@MyslPlacka>
2019-09-25 14:38:55 +0200

    cleanup, this will now be really only a planner

--- include/balloon_planner/BalloonPlanner.h
+++ include/balloon_planner/BalloonPlanner.h
@@ -39,11 +39,10 @@
 #include <mutex>
 
 // local includes
-#include <balloon_planner/eight_ukf.h>
-#include <balloon_planner/eight_rheiv.h>
 #include <balloon_planner/PlanningParamsConfig.h>
 #include <balloon_planner/ResetChosen.h>
-#include <object_detect/PoseWithCovarianceArrayStamped.h>
+#include <balloon_filter/Plane.h>
+#include <mrs_msgs/TrackerTrajectory.h>
 
 //}
 
@@ -54,23 +53,6 @@ namespace balloon_planner
   // shortcut type to the dynamic reconfigure manager template instance
   using drcfg_t = balloon_planner::PlanningParamsConfig;
   using drmgr_t = mrs_lib::DynamicReconfigureMgr<drcfg_t>;
-
-  using detections_t = object_detect::PoseWithCovarianceArrayStamped;
-  using ros_poses_t = detections_t::_poses_type;
-  using ros_pose_t = ros_poses_t::value_type::_pose_type;
-  using ros_cov_t = ros_poses_t::value_type::_covariance_type;
-
-  using RHEIV = rheiv::RHEIV;
-  using UKF = ukf::UKF;
-  using pos_t = RHEIV::x_t;
-  using cov_t = RHEIV::P_t;
-  using theta_t = RHEIV::theta_t;
-  using quat_t = Eigen::Quaterniond;
-  struct pos_cov_t
-  {
-    pos_t pos;
-    cov_t cov;
-  };
 
   /* //{ class BalloonPlanner */
 
@@ -85,8 +67,6 @@ namespace balloon_planner
     private:
       const std::string m_node_name;
       void main_loop([[maybe_unused]] const ros::TimerEvent& evt);
-      void rheiv_loop(const ros::TimerEvent& evt);
-      void prediction_loop(const ros::TimerEvent& evt);
 
     private:
       std::unique_ptr<mrs_lib::Profiler> m_profiler_ptr;
@@ -101,85 +81,19 @@ namespace balloon_planner
       std::string m_world_frame;
       std::string m_uav_frame_id;
 
-      double m_rheiv_fitting_period;
-      int m_rheiv_min_pts;
-      int m_rheiv_max_pts;
-      int m_rheiv_visualization_size;
-
-      double m_gating_distance;
-      double m_max_time_since_update;
-      double m_min_updates_to_confirm;
-      double m_z_bounds_min;
-      double m_z_bounds_max;
-
-      double m_prediction_horizon;
-      double m_prediction_step;
-
-      UKF::x_t m_process_std;
-      UKF::x_t m_init_std;
-
       //}
 
       /* ROS related variables (subscribers, timers etc.) //{ */
       std::unique_ptr<drmgr_t> m_drmgr_ptr;
       tf2_ros::Buffer m_tf_buffer;
       std::unique_ptr<tf2_ros::TransformListener> m_tf_listener_ptr;
-      mrs_lib::SubscribeHandlerPtr<detections_t> m_sh_balloons;
+      mrs_lib::SubscribeHandlerPtr<nav_msgs::Path> m_sh_predicted_path;
+      mrs_lib::SubscribeHandlerPtr<balloon_filter::Plane> m_sh_fitted_plane;
 
-      ros::Publisher m_pub_chosen_balloon;
-      ros::Publisher m_pub_used_meas;
-      ros::Publisher m_pub_pred_path;
-      ros::Publisher m_pub_plane_dbg;
-      ros::Publisher m_pub_plane_dbg2;
-      ros::Publisher m_pub_used_pts;
-
-      ros::ServiceServer m_reset_chosen_server;
+      ros::Publisher m_pub_cmd_traj;
 
       ros::Timer m_main_loop_timer;
-      ros::Timer m_rheiv_loop_timer;
-      ros::Timer m_prediction_loop_timer;
       //}
-
-      // | ----------------- RHEIV related variables ---------------- |
-
-      RHEIV m_rheiv;
-      std::mutex m_rheiv_pts_covs_mtx;
-      boost::circular_buffer<pos_t> m_rheiv_pts;
-      boost::circular_buffer<cov_t> m_rheiv_covs;
-      void add_rheiv_data(const pos_t& pos, const cov_t& cov)
-      {
-        std::scoped_lock lck(m_rheiv_pts_covs_mtx);
-        m_rheiv_pts.push_back(pos);
-        m_rheiv_covs.push_back(cov);
-      };
-      std::tuple<boost::circular_buffer<pos_t>, boost::circular_buffer<cov_t>> get_rheiv_data()
-      {
-        std::scoped_lock lck(m_rheiv_pts_covs_mtx);
-        return {m_rheiv_pts, m_rheiv_covs};
-      };
-
-      std::mutex m_rheiv_theta_mtx;
-      bool m_rheiv_theta_valid;
-      rheiv::theta_t m_rheiv_theta;
-      std::tuple<bool, rheiv::theta_t> get_rheiv_status()
-      {
-        std::scoped_lock lck(m_rheiv_theta_mtx);
-        return {m_rheiv_theta_valid, m_rheiv_theta};
-      };
-      
-      // | ------------------ UKF related variables ----------------- |
-
-      UKF m_ukf;
-      std::mutex m_ukf_estimate_mtx;
-      bool m_ukf_estimate_exists;
-      UKF::statecov_t m_ukf_estimate;
-      ros::Time m_ukf_last_update;
-      int m_ukf_n_updates;
-      std::tuple<bool, UKF::statecov_t, ros::Time, int> get_ukf_status()
-      {
-        std::scoped_lock lck(m_ukf_estimate_mtx);
-        return {m_ukf_estimate_exists, m_ukf_estimate, m_ukf_last_update, m_ukf_n_updates};
-      };
 
     private:
 
@@ -204,38 +118,6 @@ namespace balloon_planner
       }
       //}
 
-      cov_t msg2cov(const ros_cov_t& msg_cov);
-      cov_t rotate_covariance(const cov_t& covariance, const cov_t& rotation);
-      bool point_valid(const pos_t& pt);
-      quat_t plane_orientation(const theta_t& plane_theta);
-      double plane_angle(const theta_t& plane1, const theta_t& plane2);
-      pos_t plane_origin(const theta_t& plane_theta, const pos_t& origin);
-      UKF::u_t plane_theta_to_ukf_u(const theta_t& plane_theta);
-
-      /* UKF related methods //{ */
-      UKF::statecov_t predict_ukf_estimate(const ros::Time& to_stamp, const theta_t& plane_theta);
-      bool update_ukf_estimate(const std::vector<pos_cov_t>& measurements, const ros::Time& stamp, pos_cov_t& used_meas, const theta_t& plane_theta);
-      bool init_ukf_estimate(const std::vector<pos_cov_t>& measurements, const ros::Time& stamp, pos_cov_t& used_meas);
-      void reset_ukf_estimate();
-      std::vector<std::pair<UKF::x_t, ros::Time>> predict_states(const UKF::statecov_t initial_statecov, const ros::Time& initial_timestamp, const theta_t& plane_theta, const double prediction_horizon, const double prediction_step);
-      //}
-
-      rheiv::theta_t fit_plane(const boost::circular_buffer<pos_t>& points, const boost::circular_buffer<cov_t>& covs);
-
-      pos_t get_pos(const UKF::x_t& x);
-      pos_cov_t get_pos_cov(const UKF::statecov_t& statecov);
-      geometry_msgs::PoseWithCovarianceStamped to_output_message(const pos_cov_t& estimate, const std_msgs::Header& header);
-      visualization_msgs::MarkerArray to_output_message(const theta_t& plane_theta, const std_msgs::Header& header, const pos_t& origin);
-      geometry_msgs::PoseStamped to_output_message2(const theta_t& plane_theta, const std_msgs::Header& header, const pos_t& origin);
-      nav_msgs::Path to_output_message(const std::vector<std::pair<UKF::x_t, ros::Time>>& predictions, const std_msgs::Header& header, const theta_t& plane_theta);
-      sensor_msgs::PointCloud2 to_output_message(const boost::circular_buffer<pos_t>& points, const std_msgs::Header& header);
-      pos_t get_cur_mav_pos();
-      bool find_closest_to(const std::vector<pos_cov_t>& measurements, const pos_t& to_position, pos_cov_t& closest_out, bool use_gating = false);
-      bool find_closest(const std::vector<pos_cov_t>& measurements, pos_cov_t& closest_out);
-
-      std::vector<pos_cov_t> message_to_positions(const detections_t& balloon_msg);
-
-      bool reset_chosen_callback(balloon_planner::ResetChosen::Request& req, balloon_planner::ResetChosen::Response& resp);
       void load_dynparams(drcfg_t cfg);
 
   };
--- src/Lkf.cpp
+++
@@ -1,7 +0,0 @@
-#include <balloon_planner/BalloonPlanner.h>
-#include <balloon_planner/Lkf.h>
-
-namespace balloon_planner
-{
-  template class Lkf_base<lkf_n_states, lkf_n_inputs, lkf_n_measurements>;
-}  // namespace balloon_planner
